24cddfb390baaa7f7c94be77aa2ed917b377649a
venugopalreddynarala <naralavenugopalreddy@gmail.com>
2025-11-07 20:30:14 +0530

    Git: Commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
+    allowedHosts: ["trinetradigitalstudio.onrender.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
